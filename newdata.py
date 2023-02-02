f4615b5d33007087cf8387aca7329b4a0a292cc4
Nelson Mandela <77087060+Nelson-ku@users.noreply.github.com>
2023-02-02 11:51:20 +0300

    Update newdata.py

--- newdata.py
+++ newdata.py
@@ -11,7 +11,7 @@ else:
 
     
 st.header('Data analytics with streamlit')
-st.write('Streamlit is a powerful and user-friendly data analytics tool that has been gaining popularity in recent years. With Streamlit, users can create highly customizable dashboards, visualizations, and reports, all without having to write complex code or rely on a web developer.')
+st.write('Streamlit is a powerful and user-friendly data analytics tool that has been gaining popularity in recent years.')
 
 st.header('using data frame')
 st.write('Streamlit makes it easy to work with tables and display data in a tabular format. You can create tables using the st.table() function, or by using pandas DataFrames, and customize their appearance using CSS styling. Streamlit also provides features for sorting, filtering, and pagination of tables, making it a powerful tool for data analysis.')
@@ -27,7 +27,7 @@ df
 
 # dataframe=np.random.randn(10,20)
 # st.dataframe(dataframe)
-st.write('Streamlit makes it easy to highlight data points and insights in your visualizations. With its built-in support for hover text, click events, and custom styling, you can create interactive visualizations that allow you to explore your data in detail. Streamlit also provides a number of libraries and APIs that allow you to create custom highlighting effects, such as color-coding, tooltips, and animations. With these features, you can create visually appealing and informative visualizations that provide deeper insights into your data.')
+st.write('Streamlit makes it easy to highlight data points and insights in your visualizations. Streamlit also provides a number of libraries and APIs that allow you to create custom highlighting effects, such as color-coding, tooltips, and animations. With these features, you can create visually appealing and informative visualizations that provide deeper insights into your data.')
  
 st.header('highlighting data')
 dataframe= pd.DataFrame(
@@ -50,7 +50,7 @@ chart_data = pd.DataFrame(
  )
 
 st.header('using Charts in streamlit')
-st.write('Streamlit provides a simple and intuitive way to create interactive charts and visualizations for your data. With the st.line_chart, st.bar_chart, and st.area_chart functions, you can easily create line, bar, and area charts that allow you to explore your data. Streamlit also provides built-in support for more advanced visualizations, including histograms, scatter plots, and box plots. With its built-in support for popular data visualization libraries like plotly, you can also create custom visualizations that meet your specific needs. Streamlit makes it easy to create beautiful and informative visualizations that provide insights into your data, whether you are working with small or large datasets.')
+st.write('Streamlit provides a simple and intuitive way to create interactive charts and visualizations for your data. With the st.line_chart, st.bar_chart, and st.area_chart functions, you can easily create line, bar, and area charts that allow you to explore your data. Streamlit makes it easy to create beautiful and informative visualizations that provide insights into your data, whether you are working with small or large datasets.')
 st.line_chart(chart_data)
 
 st.write('plot a map using st.map() function of the san fransisco')
